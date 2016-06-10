e33e849790b9fc010f9ada46d2cd1f199ffd2de6
Pravin Sankeshwari <pravin.sankeshwari@gmail.com>
2016-06-10 21:56:56 +0530

    Add Project

---
+++ ExpandableTableview/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ExpandableTableview
+//
+//  Created by Pravin Sankeshwari on 10/06/16.
+//  Copyright © 2016 Pravin Sankeshwari. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
